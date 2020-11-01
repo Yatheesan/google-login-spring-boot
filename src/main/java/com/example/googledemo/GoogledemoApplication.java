137e28a15a81450da9f04e3f918bee269cfe795f
YatheesanC <>
2020-11-01 12:31:32 +0530

    initial commit for google login

---
+++ src/main/java/com/example/googledemo/GoogledemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.googledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogledemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/googledemo/GoogledemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogledemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
